6d66bbf91d6226def0b01b70d4bfb8e05dc66a16
ermero <ermero@floridauniversitaria.es>
2016-11-19 11:16:15 +0100

    version inicial

---
+++ src/FCTBundle/Controller/ApiController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace FCTBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiController extends Controller
+{
+	function empresaToArray($empresa){
+		$nombre=$empresa->getNombre();
+		$direccion=$empresa->getDireccion();
+		$cp=$empresa->getCp();
+		$telefon1=$empresa->getTelefono1();
+		$telefon2=$empresa->getTelefono2();
+		$creacion=$empresa->getFechaCreacion();
+
+		return array('nombre'=>$nombre,'direccion'=>$direccion,'cp'=>$cp,'telefono1'=>$telefon1,'telefono2'=>$telefon2,'fecha'=>$creacion);
+	}
+    public function getAction()
+    {
+    	$repository = $this->getDoctrine()->getRepository('FCTBundle:Empresa');
+        $empresas = $repository->findAll();
+        $data = array('empresas' => array());
+        foreach ($empresas as $empresa) {
+            $data['empresas'][] = $this->empresaToArray($empresa);
+        }
+
+        $response = new JsonResponse($data, 400);
+        return $response;
+        //return $this->json($empresas);*/
+    }
+     public function postAction()
+    {
+        return $this->render('FCTBundle:Default:index.html.twig');
+    }
+}
